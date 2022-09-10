1f425824b94179582fe6b3c17a10af8e57067953
akira-wd-1224 <jda.7seven.a24@gmail.com>
2022-09-11 02:40:28 +0900

    ✨ MantineProviderの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
-import '../styles/globals.css'
-import "src/lib/tailwind.css"
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "src/lib/tailwind.css";
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
