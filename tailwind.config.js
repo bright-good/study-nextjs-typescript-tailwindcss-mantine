2114cb838dfc91a5046dcbb5e56da5a5907cde7a
akira-wd-1224 <jda.7seven.a24@gmail.com>
2022-09-11 03:57:05 +0900

    🔧 mantineのレスポンシブに合わせる

--- tailwind.config.js
+++ tailwind.config.js
@@ -174,6 +174,14 @@ const mantineColors = {
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    /* Mantineと一致させる: https://mantine.dev/theming/responsive */
+    screens: {
+      xs: "576px",
+      sm: "768px",
+      md: "992px",
+      lg: "1200px",
+      xl: "1400px",
+    },
     extend: {
       colors: mantineColors,
     },
