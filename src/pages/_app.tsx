67206e0f994801966e706d231c62d35bc24e0079
akira-wd-1224 <jda.7seven.a24@gmail.com>
2022-09-11 03:12:15 +0900

    ✨ mantineBase.cssのインポート

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import "../styles/globals.css";
+import "src/lib/mantineBase.css";
 import "src/lib/tailwind.css";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
 
